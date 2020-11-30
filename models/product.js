643e247f146492cebfe7f6e12b8aa5d171fb9f5b
Atchett <Atchett@users.noreply.github.com>
2020-11-30 20:16:49 +0000

    initial commit at mvc

---
+++ models/product.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const pathHelper = require("../util/path");
+const p = path.join(pathHelper, "data", "products.json");
+
+// pass in the callback function that
+// will run when the file has been read
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
