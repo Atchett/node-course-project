6f7eb0f24d188992169ae3905e8bbf26201de1df
Atchett <Atchett@users.noreply.github.com>
2020-12-14 13:50:03 +0000

    adding basic Stripe payment functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -246,6 +248,73 @@ exports.getInvoice = (req, res, next) => {
     });
 };
 
-// exports.getCheckout = (req, res, next) => {
-//   res.render("shop/checkout", { path: "/checkout", pageTitle: "Checkout" });
-// };
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+    .populate("cart.items.product")
+    .execPopulate()
+    .then((user) => {
+      products = user.cart.items;
+      total = products.reduce((t, p) => t + p.quantity * p.product.price, 0);
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => {
+          return {
+            name: p.product.title,
+            description: p.product.description,
+            amount: p.product.price * 100,
+            currency: "usd",
+            quantity: p.quantity,
+          };
+        }),
+        success_url: `${req.protocol}://${req.get("host")}/checkout/success`,
+        cancel_url: `${req.protocol}://${req.get("host")}/checkout/cancel`,
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+        stripeKey: process.env.STRIPE_KEY,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(`getCheckout failed. Error: ${err}`);
+      error.httpStatusCode = "500";
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.product")
+    .execPopulate()
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.product._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
+      const error = new Error(`postOrder failed. Error: ${err}`);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
